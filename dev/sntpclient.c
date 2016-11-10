51e7ec51addd9c465f3956df51de37a4d54876dd
Sam Gasper <samuel2.gasper@live.uwe.ac.uk>
2016-11-10 23:13:38 +0000

    Dev

--- dev/sntpclient.c
+++ dev/sntpclient.c
@@ -2,6 +2,7 @@
  * need to supply host name/IP and one word message,
  * e.g. talker localhost hello
  */
+#include "timeconvertion.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,12 +12,13 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <time.h>
 #include <netdb.h>         /* for gethostbyname() */
 
 #define PORT 123     /* server port the client connects to */
 #define MAXBUFLEN 200
 
-//#define NTP_EPOCH       (86400U * (365U * 70U + 17U))
+#define NTP_EPOCH       (86400U * (365U * 70U + 17U))
 
 struct ntp_packet {
  // unsigned version;
@@ -37,57 +39,49 @@ struct ntp_packet {
   uint32_t transmit_timestamp_fraq;
 };
 
-struct ntp_time_t {
-    uint32_t   second;
-    uint32_t    fraction;
-};
-
-/*struct timeval {
-    uint32_t   tv_sec;
-    uint32_t    tv_usec;
-};*/
-
-void convert_ntp_time_into_unix_time(struct ntp_time_t *ntp, struct timeval *nix)
-{
-    nix->tv_sec = ntp->second - 0x83AA7E80; // the seconds from Jan 1, 1900 to Jan 1, 1970
-    nix->tv_usec = (uint32_t)( (double)ntp->fraction * 1.0e6 / (double)(1LL<<32) );
-}
+void create_packet(struct ntp_packet *pkt);
+int initialise_connection_to_server(char hostanme[],
+                                    struct sockaddr_in *their_addr,
+                                    int *sockfd);
+int process_cmdline(int argc, char * argv[]);
 
 int main( int argc, char * argv[]) {
   int sockfd, numbytes;
   int addr_len;
   struct ntp_packet pkt;
-  struct hostent *he;
+  //struct hostent *he;
   struct sockaddr_in their_addr;    /* server address info */
 
   struct ntp_time_t ref_time;
-  struct timeval nix;
+  struct timeval epoch;
 
-  memset( &pkt, 0, sizeof pkt  );
-  // set SNTP V4 and Mode 3(client)
-  pkt.li_vn_mode = (4 << 3) | 3; // (vn << 3) | mode
-  printf( "Mode of packet to send: %u\n", pkt.li_vn_mode);
-//  pkt.transmit_timestamp_secs = time(0) + NTP_EPOCH;
-
-  if( argc != 2) {
-    fprintf( stderr, "usage: simpleclient hostname\n");
-    exit( 1);
+  // check cmd line arguments
+  if (process_cmdline(argc, argv) != 0){
+    exit(1);
   }
+
+  if (initialise_connection_to_server(argv[ 1], &their_addr, &sockfd) != 0){
+    exit(1);
+  }
+
   /* resolve server host name or IP address */
-  if( (he = gethostbyname( argv[ 1])) == NULL) {
-    perror( "simpleclient gethostbyname");
-    exit( 1);
-  }
+  //if( (he = gethostbyname( argv[ 1])) == NULL) {
+  //  perror( "simpleclient gethostbyname");
+  //  exit( 1);
+  //}
 
-  if( (sockfd = socket( AF_INET, SOCK_DGRAM, 0)) == -1) {
-    perror( "simpleclient socket");
-    exit( 1);
-  }
+//  if( (sockfd = socket( AF_INET, SOCK_DGRAM, 0)) == -1) {
+//    perror( "simpleclient socket");
+//    exit( 1);
+//  }
 
-  memset( &their_addr,0, sizeof(their_addr)); /* zero struct */
-  their_addr.sin_family = AF_INET;    /* host byte order .. */
-  their_addr.sin_port = htons( PORT); /* .. short, netwk byte order */
-  their_addr.sin_addr = *((struct in_addr *)he -> h_addr);
+  //memset( &their_addr,0, sizeof(their_addr)); /* zero struct */
+//  their_addr.sin_family = AF_INET;    /* host byte order .. */
+  //their_addr.sin_port = htons( PORT); /* .. short, netwk byte order */
+  //their_addr.sin_addr = *((struct in_addr *)he -> h_addr);
+
+
+  create_packet(&pkt);
 
   // send NTP packet
   if( (numbytes = sendto( sockfd, &pkt, 48, 0, //48 TODO: make sizeof pkt work
@@ -112,11 +106,51 @@ int main( int argc, char * argv[]) {
 
   ref_time.second = ntohl(pkt.reference_timestamp_secs);
   ref_time.fraction = ntohl(pkt.reference_timestamp_fraq);
-  convert_ntp_time_into_unix_time(&ref_time, &nix);
-  printf("NTP time: %ld %ld\n", ref_time.second, ref_time.fraction);
-  printf("UNIX Time: %ld %ld\n", nix.tv_sec, nix.tv_usec);
+  convert_ntp_time_into_unix_time(&ref_time, &epoch);
+  printf("NTP time: %u %u\n", ref_time.second, ref_time.fraction);
+  printf("UNIX Time: %ld %ld\n", epoch.tv_sec, epoch.tv_usec);
 //  printf( "Ref timestamp is: %u\n", pkt.secs
 
   close( sockfd);
   return 0;
 }
+
+ void create_packet(struct ntp_packet *pkt){
+   memset( pkt, 0, sizeof *pkt  );
+   // set SNTP V4 and Mode 3(client)
+   pkt->li_vn_mode = (4 << 3) | 3; // (vn << 3) | mode
+   pkt->transmit_timestamp_secs = htonl(time(0) + NTP_EPOCH);
+   // TODO: do fraq/miliseconds
+ }
+
+ int initialise_connection_to_server(char hostname[],
+                                     struct sockaddr_in *their_addr,
+                                     int *sockfd){
+   struct hostent *he;
+
+   /* resolve server host name or IP address */
+   if( (he = gethostbyname( hostname)) == NULL) {
+     perror( "simpleclient gethostbyname");
+     return 1;
+   }
+
+   if( (*sockfd = socket( AF_INET, SOCK_DGRAM, 0)) == -1) {
+     perror( "simpleclient socket");
+     return 1;
+   }
+
+   memset( their_addr,0, sizeof(their_addr)); /* zero struct */
+   their_addr->sin_family = AF_INET;    /* host byte order .. */
+   their_addr->sin_port = htons( PORT); /* .. short, netwk byte order */
+   their_addr->sin_addr = *((struct in_addr *)he -> h_addr);
+
+   return 0;
+ }
+
+ int process_cmdline(int argc, char * argv[]){
+   if( argc != 2) {
+     fprintf( stderr, "usage: simpleclient hostname\n");
+     return 1;
+   }
+   return 0;
+ }
--- dev/timeconvertion.c
+++ dev/timeconvertion.c
@@ -1,25 +1,27 @@
-#include <sys/time.h>
+/*#include <sys/time.h>
 #include <stdio.h>
 #include <stdint.h>
 
 struct ntp_time_t {
     uint32_t   second;
     uint32_t    fraction;
-};
+};*/
 
-void convert_ntp_time_into_nix_time(struct ntp_time_t *ntp, struct timeval *nix)
+#include "timeconvertion.h"
+
+void convert_ntp_time_into_unix_time(struct ntp_time_t *ntp, struct timeval *epoch)
 {
-    nix->tv_sec = ntp->second - 0x83AA7E80; // the seconds from Jan 1, 1900 to Jan 1, 1970
-    nix->tv_usec = (uint32_t)( (double)ntp->fraction * 1.0e6 / (double)(1LL<<32) );
+    epoch->tv_sec = ntp->second - 0x83AA7E80; // the seconds from Jan 1, 1900 to Jan 1, 1970
+    epoch->tv_usec = (uint32_t)( (double)ntp->fraction * 1.0e6 / (double)(1LL<<32) );
 }
 
-void convert_nix_time_into_ntp_time(struct timeval *nix, struct ntp_time_t *ntp)
+void convert_nix_time_into_ntp_time(struct timeval *epoch, struct ntp_time_t *ntp)
 {
-    ntp->second = nix->tv_sec + 0x83AA7E80;
-    ntp->fraction = (uint32_t)( (double)(nix->tv_usec+1) * (double)(1LL<<32) * 1.0e-6 );
+    ntp->second = epoch->tv_sec + 0x83AA7E80;
+    ntp->fraction = (uint32_t)( (double)(epoch->tv_usec+1) * (double)(1LL<<32) * 1.0e-6 );
 }
 
-int main()
+/*int main()
 {
     struct ntp_time_t ntp;
     struct timeval nix;
@@ -35,4 +37,4 @@ int main()
     // convert ntp time back to nix time to see whether they are same
     convert_ntp_time_into_nix_time(&ntp, &nix);
     printf("UNIX Time: %ld %ld\n", nix.tv_sec, nix.tv_usec);
-}
+}*/
