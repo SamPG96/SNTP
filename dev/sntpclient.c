4134bca6c9d2134a6acb7f8dcfef16924a92dd97
Sam Gasper <samuel2.gasper@live.uwe.ac.uk>
2016-11-11 21:12:50 +0000

    Dev

--- dev/sntpclient.c
+++ dev/sntpclient.c
@@ -42,17 +42,18 @@ struct ntp_packet {
 
 void create_packet(struct ntp_packet *pkt);
 int initialise_connection_to_server(char hostanme[],
-                                    struct sockaddr_in *their_addr,
+                                    struct sockaddr_in *their_addr, int *sockfd);
+int recieve_SNTP_packet(struct ntp_packet *pkt, struct sockaddr_in *their_addr,
                                     int *sockfd);
-int send_SNTP_packet(struct ntp_packet *pkt,
-                     struct sockaddr_in *their_addr,
+int send_SNTP_packet(struct ntp_packet *pkt, struct sockaddr_in *their_addr,
                      int *sockfd);
 int process_cmdline(int argc, char * argv[]);
+void print_epoch_time_in_human_readable_form(long secs, long fraq);
 
 int main( int argc, char * argv[]) {
-  int sockfd, numbytes;
-  int addr_len;
-  struct ntp_packet pkt;
+  int sockfd;
+  struct ntp_packet request_pkt; // request from client to server
+  struct ntp_packet response_pkt; // response from server to client
   struct sockaddr_in their_addr;    /* server address info */
 
   struct ntp_time_t ref_time;
@@ -68,45 +69,37 @@ int main( int argc, char * argv[]) {
     exit(1);
   }
 
-  // build sntp packet
-  create_packet(&pkt);
+  // build sntp request packet
+  create_packet(&request_pkt);
 
-  // send NTP packet
-  if (send_SNTP_packet() != 0){
-    exit(1)
-  }
-  /*if( (numbytes = sendto( sockfd, &pkt, 48, 0, //48 TODO: make sizeof pkt work
-      (struct sockaddr *)&their_addr, sizeof( struct sockaddr))) == -1) {
-      perror( "Talker sendto");
-      exit( 1);
+  // send request packet to server
+  if (send_SNTP_packet(&request_pkt, &their_addr, &sockfd) != 0){
+    exit(1);
   }
 
-  printf( "Sent %d bytes to %s\n", numbytes,
-                          inet_ntoa( their_addr.sin_addr));*/
-
-  // get response packet
-  addr_len = sizeof( struct sockaddr);
-  if( (numbytes = recvfrom( sockfd, &pkt, MAXBUFLEN - 1, 0,
-                (struct sockaddr *)&their_addr, &addr_len)) == -1) {
-      perror( "Listener recvfrom");
-      exit( 1);
+  // recieve NTP response packet from server
+  if (recieve_SNTP_packet(&response_pkt, &their_addr, &sockfd) != 0){
+    exit(1);
   }
 
-  printf( "Got packet from %s\n", inet_ntoa( their_addr.sin_addr));
-  printf( "Recieved packet is %d bytes long\n", numbytes);
+  // TODO: run checks on packet here
 
-  ref_time.second = ntohl(pkt.reference_timestamp_secs);
-  ref_time.fraction = ntohl(pkt.reference_timestamp_fraq);
+  ref_time.second = ntohl(response_pkt.reference_timestamp_secs);
+  ref_time.fraction = ntohl(response_pkt.reference_timestamp_fraq);
   convert_ntp_time_into_unix_time(&ref_time, &epoch);
   printf("NTP time: %u %u\n", ref_time.second, ref_time.fraction);
   printf("UNIX Time: %ld %ld\n", epoch.tv_sec, epoch.tv_usec);
 //  printf( "Ref timestamp is: %u\n", pkt.secs
 
+
+  print_epoch_time_in_human_readable_form(epoch.tv_sec, epoch.tv_usec);
+
   close( sockfd);
   return 0;
 }
 
- void create_packet(struct ntp_packet *pkt){
+
+void create_packet(struct ntp_packet *pkt){
    memset( pkt, 0, sizeof *pkt  );
    // set SNTP V4 and Mode 3(client)
    pkt->li_vn_mode = (4 << 3) | 3; // (vn << 3) | mode
@@ -114,7 +107,18 @@ int main( int argc, char * argv[]) {
    // TODO: do fraq/miliseconds
  }
 
- int initialise_connection_to_server(char hostname[],
+
+void print_epoch_time_in_human_readable_form(long secs, long fraq){
+  char readable_time_string[30];
+  struct tm  ts;
+
+  ts = *localtime(&secs);
+  strftime(readable_time_string, sizeof(readable_time_string), "%Y-%m-%d %H:%M:%S", &ts);
+  printf("%s.%ld\n", readable_time_string, fraq);
+}
+
+
+int initialise_connection_to_server(char hostname[],
                                      struct sockaddr_in *their_addr,
                                      int *sockfd){
    struct hostent *he;
@@ -130,7 +134,7 @@ int main( int argc, char * argv[]) {
      return 1;
    }
 
-   memset( their_addr,0, sizeof(their_addr)); /* zero struct */
+   memset( their_addr,0, sizeof *their_addr); /* zero struct */
    their_addr->sin_family = AF_INET;    /* host byte order .. */
    their_addr->sin_port = htons( PORT); /* .. short, netwk byte order */
    their_addr->sin_addr = *((struct in_addr *)he -> h_addr);
@@ -138,17 +142,37 @@ int main( int argc, char * argv[]) {
    return 0;
  }
 
+
+int recieve_SNTP_packet(struct ntp_packet *pkt, struct sockaddr_in *their_addr,
+                                     int *sockfd){
+  int addr_len;
+  int numbytes;
+  addr_len = sizeof( struct sockaddr);
+  if( (numbytes = recvfrom( *sockfd, pkt, MAXBUFLEN - 1, 0,
+               (struct sockaddr *)their_addr, &addr_len)) == -1) {
+    perror( "Listener recvfrom");
+    return  1;
+  }
+
+  printf( "Got packet from %s\n", inet_ntoa( their_addr->sin_addr));
+  printf( "Recieved packet is %d bytes long\n", numbytes);
+  return 0;
+}
+
+
 int send_SNTP_packet(struct ntp_packet *pkt, struct sockaddr_in *their_addr,
                           int *sockfd){
-  if( (numbytes = sendto( sockfd, &pkt, 48, 0, //48 TODO: make sizeof pkt work
-      (struct sockaddr *)&their_addr, sizeof( struct sockaddr))) == -1) {
+  int numbytes;
+  if( (numbytes = sendto( *sockfd, pkt, 48, 0, //48 TODO: make sizeof pkt work
+      (struct sockaddr *)their_addr, sizeof( struct sockaddr))) == -1) {
     perror( "Talker sendto");
     return  1;
   }
   printf( "Sent %d bytes to %s\n", numbytes,
-                          inet_ntoa( their_addr.sin_addr));
+                          inet_ntoa( their_addr->sin_addr));
   return 0;
 }
+
 
 int process_cmdline(int argc, char * argv[]){
    if( argc != 2) {
---
+++ dev/timeconvertion.h
@@ -0,0 +1,11 @@
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdint.h>
+
+struct ntp_time_t {
+    uint32_t   second;
+    uint32_t    fraction;
+};
+
+void convert_ntp_time_into_unix_time(struct ntp_time_t *ntp, struct timeval *epoch);
+void convert_nix_time_into_ntp_time(struct timeval *epoch, struct ntp_time_t *ntp);
